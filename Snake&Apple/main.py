44168530be22165d3b96ba8a96c4f69e56e68f87
vagabondraj <raviraj06112005@gmail.com>
2026-01-07 23:35:32 +0530

    Keys Movement Added

--- Snake&Apple/main.py
+++ Snake&Apple/main.py
@@ -2,18 +2,42 @@ import pygame
 from pygame.locals import *
 
 
+def draw_block():
+    surface.fill((0, 0, 0))
+    surface.blit(block, (block_x, block_y))
+    pygame.display.flip()
+
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((640, 480))
-    pygame.display.set_caption("Snake & Apple Game")
 
+    surface = pygame.display.set_mode((700, 700))
+    pygame.display.set_caption("Snake & Apple")
+    surface.fill((0, 0, 0))
+
+    block = pygame.image.load("./Resources/block.jpg").convert()
+    block_x, block_y = 300, 300
+    block = pygame.transform.scale(block, (40, 40))
+    surface.blit(block, (block_x, block_y))
+
+    pygame.display.flip()
+    
     running = True
     while running:
         for event in pygame.event.get():
-            if event.type == QUIT:
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    running = False
+                if event.key == K_UP:
+                    block_y -= 20
+                    draw_block()
+                if event.key == K_DOWN:
+                    block_y += 20
+                    draw_block()
+                if event.key == K_LEFT:
+                    block_x -= 20
+                    draw_block()
+                if event.key == K_RIGHT:
+                    block_x += 20
+                    draw_block()
+            elif event.type == QUIT:
                 running = False
-
-        screen.fill((0, 0, 0))  # Clear screen with black
-        pygame.display.flip()
-
-    pygame.quit()
