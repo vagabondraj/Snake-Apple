1bb32a052eb9e0b037f11841d8209745a42684e5
vagabondraj <raviraj06112005@gmail.com>
2026-01-10 23:47:29 +0530

    Snake len Increase and apple random appearance

--- Snake&Apple/main.py
+++ Snake&Apple/main.py
@@ -1,21 +1,25 @@
 import pygame
 from pygame.locals import *
+import random
 
+# ===================== CONSTANTS =====================
+SCREEN_SIZE = 700
+BLOCK_SIZE = 20
+GRID_SIZE = SCREEN_SIZE // BLOCK_SIZE
+GAME_SPEED = 150
 
+# ===================== GAME CLASS =====================
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((700, 700))
+        self.surface = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
         pygame.display.set_caption("Snake & Apple")
 
         self.snake = Snake(self.surface, 3)
         self.apple = Apple(self.surface)
-    
-    def play(self):
-        self.snake.walk()
-        self.apple.draw()
-        self.snake.draw()
-        pygame.display.flip()
+
+    def is_collision(self, x1, y1, x2, y2):
+        return x1 == x2 and y1 == y2
 
     def run(self):
         running = True
@@ -35,42 +39,57 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
+            # Move snake
             self.snake.walk()
 
-            # DRAW ORDER MATTERS
+            # Collision with apple
+            if self.is_collision(
+                self.snake.x[0], self.snake.y[0],
+                self.apple.x, self.apple.y
+            ):
+                self.snake.increase_length()
+                self.apple.move()
+
+            # Draw everything
             self.surface.fill((0, 0, 0))
             self.snake.draw()
             self.apple.draw()
-
             pygame.display.flip()
-            pygame.time.delay(150)
+
+            pygame.time.delay(GAME_SPEED)
 
         pygame.quit()
 
-
+# ===================== APPLE CLASS =====================
 class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
         self.image = pygame.image.load("./Resources/apple.jpg").convert()
-        self.image = pygame.transform.scale(self.image, (20, 20))
-
-        self.x = 100
-        self.y = 100
+        self.image = pygame.transform.scale(
+            self.image, (BLOCK_SIZE, BLOCK_SIZE)
+        )
+        self.move()
 
     def draw(self):
         self.parent_screen.blit(self.image, (self.x, self.y))
 
+    def move(self):
+        self.x = random.randint(0, GRID_SIZE - 1) * BLOCK_SIZE
+        self.y = random.randint(0, GRID_SIZE - 1) * BLOCK_SIZE
 
+# ===================== SNAKE CLASS =====================
 class Snake:
     def __init__(self, parent_screen, length):
         self.parent_screen = parent_screen
         self.length = length
 
         self.original_block = pygame.image.load("./Resources/block.jpg").convert()
-        self.original_block = pygame.transform.scale(self.original_block, (20, 20))
+        self.original_block = pygame.transform.scale(
+            self.original_block, (BLOCK_SIZE, BLOCK_SIZE)
+        )
         self.block = self.original_block
 
-        self.x = [300 - i * 20 for i in range(length)]
+        self.x = [300 - i * BLOCK_SIZE for i in range(length)]
         self.y = [300] * length
 
         self.direction = "RIGHT"
@@ -79,29 +98,30 @@ class Snake:
         for i in range(self.length):
             self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
 
+    def increase_length(self):
+        self.length += 1
+        self.x.append(self.x[-1])
+        self.y.append(self.y[-1])
+
     def move_up(self):
-        if self.direction == "DOWN":
-            return
-        self.direction = "UP"
-        self.block = pygame.transform.rotate(self.original_block, 90)
+        if self.direction != "DOWN":
+            self.direction = "UP"
+            self.block = pygame.transform.rotate(self.original_block, 90)
 
     def move_down(self):
-        if self.direction == "UP":
-            return
-        self.direction = "DOWN"
-        self.block = pygame.transform.rotate(self.original_block, -90)
+        if self.direction != "UP":
+            self.direction = "DOWN"
+            self.block = pygame.transform.rotate(self.original_block, -90)
 
     def move_left(self):
-        if self.direction == "RIGHT":
-            return
-        self.direction = "LEFT"
-        self.block = pygame.transform.rotate(self.original_block, 180)
+        if self.direction != "RIGHT":
+            self.direction = "LEFT"
+            self.block = pygame.transform.rotate(self.original_block, 180)
 
     def move_right(self):
-        if self.direction == "LEFT":
-            return
-        self.direction = "RIGHT"
-        self.block = self.original_block
+        if self.direction != "LEFT":
+            self.direction = "RIGHT"
+            self.block = self.original_block
 
     def walk(self):
         for i in range(self.length - 1, 0, -1):
@@ -109,15 +129,15 @@ class Snake:
             self.y[i] = self.y[i - 1]
 
         if self.direction == "UP":
-            self.y[0] -= 20
+            self.y[0] -= BLOCK_SIZE
         elif self.direction == "DOWN":
-            self.y[0] += 20
+            self.y[0] += BLOCK_SIZE
         elif self.direction == "LEFT":
-            self.x[0] -= 20
+            self.x[0] -= BLOCK_SIZE
         elif self.direction == "RIGHT":
-            self.x[0] += 20
+            self.x[0] += BLOCK_SIZE
 
-
+# ===================== MAIN =====================
 if __name__ == "__main__":
     game = Game()
     game.run()
